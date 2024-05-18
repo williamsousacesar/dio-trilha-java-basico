4d23be6e397db2a40a055cdae99da94962f2b67a
William de Sousa Cesasr <william.cesar@gmail.com>
2024-05-18 17:00:25 -0300

    Resposta desafio Controle de Fluxo - Desafio

---
+++ Comentarios/src/Comentarios.java
@@ -0,0 +1,25 @@
+public class Comentarios {
+
+    public static void man (String[] args) {
+        //Olá eu sou um comentário em uma unica linha
+
+        /*Olá,
+         * Eu sou um comentário
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+
+         /**
+          * Estas duas estrelinhas acima
+          *é para identificar que você
+          *pretende elaborar um comentário
+          *a nivel de documentação.
+          *Que incrível!!!
+          */
+          public void metodo() {
+
+          }
+
+
+    }
+}
